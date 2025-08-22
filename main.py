8b1018e6f044e56b4c8c5add14792a85a93efcfb
VaishnaviNayase <vaishnavinayase@gmail.com>
2025-08-22 15:10:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,5 +13,5 @@ app.include_router(likes.router, prefix="/likes", tags=["Likes"])
 app.include_router(recent_viewed.router, prefix="/viewed", tags=["Recently Viewed"])
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 10000))
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
