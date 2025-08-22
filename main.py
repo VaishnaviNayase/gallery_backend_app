cdaf5151c82e1134fea9275751a3abc5f43de18b
VaishnaviNayase <vaishnavinayase@gmail.com>
2025-08-22 15:01:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from routers import users, likes, recent_viewed
-
+import os
 app = FastAPI(title="Dog Gallery API")
 
 @app.get("/")
@@ -11,3 +11,7 @@ app.include_router(users.router, prefix="/users", tags=["Users"])
 app.include_router(likes.router, prefix="/like", tags=["Like"])
 app.include_router(likes.router, prefix="/likes", tags=["Likes"])
 app.include_router(recent_viewed.router, prefix="/viewed", tags=["Recently Viewed"])
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
