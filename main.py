561559779e43f44d837b1f123ea1ef44c434928c
VaishnaviNayase <vaishnavinayase@gmail.com>
2025-08-22 16:29:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
 from routers import users, likes, recent_viewed
+from fastapi.middleware.cors import CORSMiddleware
+
 import os
 app = FastAPI(title="Dog Gallery API")
-
+origins = [
+        "*"
+]
+app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],  # Allows all HTTP methods (GET, POST, PUT, DELETE, OPTIONS, etc.)
+        allow_headers=["*"],  # Allows all headers in requests
+)
 @app.get("/")
 def root():
     return {"message": "Dog Gallery API running "}
